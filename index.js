7e264ad2db2da9ac7672a08a2b9206e6ec063917
dislersd <dislersd@gmail.com>
2019-03-18 13:43:44 -0700

    set up server with express and created endpoint to get all users from database

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 // implement your API here
 
+const express = require("express");
+const db = require("./data/db.js");
+const server = express();
 
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('hello')
+});
+
+server.get('/now', (req, res) => {
+  res.send( new Date());
+});
+
+server.get("/users", (req, res) => {
+  db
+    .find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "error retrieving users" });
+    });
+});
+
+server.listen(3000, () => {
+  console.log(`\n** Up and running on port 3K`);
+});
